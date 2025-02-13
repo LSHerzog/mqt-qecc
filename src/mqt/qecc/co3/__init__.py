4c558235a9615cc253332cff949ff97d4180ef86
LSHerzog <laura.s.herzog@hotmail.de>
2025-02-13 12:38:22 +0100

    :white_check_mark: added tests and update init

--- src/mqt/qecc/co3/__init__.py
+++ src/mqt/qecc/co3/__init__.py
@@ -1,14 +1,18 @@
 """Co3."""
 from __future__ import annotations
 
+from .microscopic.snake_builder import SnakeBuilder, SnakeBuilderSTDW
 from .utils.hill_climber import HillClimbing
-from .utils.lattice_router import HexagonalLattice, ShortestFirstRouter
+from .utils.lattice_router import HexagonalLattice, ShortestFirstRouter, ShortestFirstRouterTGates
 from .utils.misc import generate_random_circuit, translate_layout_circuit
 
 __all__ = [
     "HexagonalLattice",
     "HillClimbing",
     "ShortestFirstRouter",
+    "ShortestFirstRouterTGates",
+    "SnakeBuilder",
+    "SnakeBuilderSTDW",
     "generate_random_circuit",
     "translate_layout_circuit"
 ]
