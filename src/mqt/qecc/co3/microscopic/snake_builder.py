fe05d89bf5316b0fdd4ba56631959f47acf820fb
LSHerzog <laura.s.herzog@hotmail.de>
2025-04-07 13:12:28 +0200

    removed assertions to allow d=3 snakebuilder stdw

--- src/mqt/qecc/co3/microscopic/snake_builder.py
+++ src/mqt/qecc/co3/microscopic/snake_builder.py
@@ -1242,7 +1242,7 @@ class SnakeBuilderSTDW:
    #------------methods for ZLZL stabilizer subset---------------
     def find_outer_bdry(self) -> list[tuple]:
         """Finds the set of outer boundaries of the triangles (not connected to stdw) for the inner triangles (start and end triangle not included)."""
-        assert self.d > 3, "This construction only works for d>=5."
+        #assert self.d > 3, "This construction only works for d>=5."
         triangles_to_check = list(range(1, len(self.positions)-1))
         outer_nodes_total = []
         for triangle in triangles_to_check:
@@ -1270,7 +1270,7 @@ class SnakeBuilderSTDW:
                 closest_corners.extend(corner for corner in lst_corner if corner in shortest_paths)
             outer_nodes += list(set(closest_corners))
             outer_nodes = list(set(outer_nodes)) #remove duplicates
-            assert len(outer_nodes) == self.d, "The number of nodes on the outer bundary must be the same as the distance."
+            #assert len(outer_nodes) == self.d, "The number of nodes on the outer bundary must be the same as the distance."
             outer_nodes_total.append(outer_nodes)
         self.outer_nodes_total = outer_nodes_total
         return outer_nodes_total
